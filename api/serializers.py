a81f25e4eb3a0d3d8a4a3c296b13a2778ea1f447
HuyTran <huy.tran@lundimatin.fr>
2024-11-26 15:22:03 +0700

    Revert "Update Feature Product"
    This reverts commit ec43ce75c3ae2a80453a68ef96c6fd6f3f6949ec.

--- api/serializers.py
+++ api/serializers.py
@@ -177,6 +177,7 @@ class CategorySerializer(serializers.ModelSerializer):
             'is_active', 'meta_title', 'meta_description', 'sort_order'
         )
 
+
 class ProductSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), required=False, allow_null=True)
@@ -201,14 +202,6 @@ class ProductSerializer(serializers.ModelSerializer):
         product.tags.set(tags)
         return product
 
-    def update(self, instance, validated_data):
-        tags = validated_data.pop('tags', None)
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        instance.save()
-        if tags is not None:
-            instance.tags.set(tags)
-        return instance
 
 class PermissionSerializer(serializers.ModelSerializer):
     class Meta:
