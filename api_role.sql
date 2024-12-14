4af7bc63385286f059d0b25dbc7c387b9516d312
HuyTran <huy.tran@lundimatin.fr>
2024-12-14 10:41:07 +0700

    New Migration

---
+++ api_role.sql
@@ -0,0 +1,4 @@
+INSERT INTO api_role (id, name, description) VALUES (1, 'ADMIN', 'ADMIN');
+INSERT INTO api_role (id, name, description) VALUES (2, 'SELLER', 'SELLER');
+INSERT INTO api_role (id, name, description) VALUES (5, 'CUSTOMER 2', 'customer');
+INSERT INTO api_role (id, name, description) VALUES (6, 'ASSISTANT', 'ASSISTANT');
