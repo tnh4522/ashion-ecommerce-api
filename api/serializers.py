27e607412278e99d9fcc4439eb36755c386634f4
HuyTran <huy.tran@lundimatin.fr>
2024-12-26 00:16:22 +0700

    Fix Update Role Permission

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,7 @@
-from django.db import IntegrityError, transaction
 from rest_framework import serializers
 from .models import *
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .product.product_serializers import ProductSerializer
-
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
@@ -182,7 +180,6 @@ class CategorySerializer(serializers.ModelSerializer):
             'is_active', 'meta_title', 'meta_description', 'sort_order'
         )
 
-
 class PermissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Permission
@@ -242,72 +239,6 @@ class RoleSerializer(serializers.ModelSerializer):
         RolePermission.objects.bulk_create(role_permissions)
 
         return role
-
-
-class RolePermissionUpdateSerializer(serializers.Serializer):
-    permissions = serializers.ListField(
-        child=serializers.CharField(),
-        help_text="List of permissions in the format 'model:action'."
-    )
-
-    def validate_permissions(self, value):
-        """
-        Validate that each permission string is in the correct format and exists in the database.
-        """
-        valid_permissions = []
-        invalid_permissions = []
-        for perm_str in value:
-            try:
-                model_name, action = perm_str.split(':')
-            except ValueError:
-                invalid_permissions.append(perm_str)
-                continue
-
-            try:
-                permission = Permission.objects.get(model_name=model_name, action=action)
-                valid_permissions.append(permission)
-            except Permission.DoesNotExist:
-                invalid_permissions.append(perm_str)
-
-        if invalid_permissions:
-            raise serializers.ValidationError(
-                f"The following permissions are invalid or do not exist: {', '.join(invalid_permissions)}"
-            )
-
-        return valid_permissions
-
-    def update_permissions(self, role, permissions):
-        """
-        Update the role's permissions:
-        - Set `allowed=True` for permissions in the input list.
-        - Set `allowed=False` for all other permissions not in the input list.
-        """
-        input_permission_ids = set(permission.id for permission in permissions)
-
-        existing_role_permissions = RolePermission.objects.filter(role=role)
-        existing_permission_ids = set(existing_role_permissions.values_list('permission_id', flat=True))
-
-        permissions_to_add_ids = input_permission_ids - existing_permission_ids
-        permissions_to_add = Permission.objects.filter(id__in=permissions_to_add_ids)
-
-        permissions_to_update_ids = input_permission_ids & existing_permission_ids
-        permissions_to_deny_ids = existing_permission_ids - input_permission_ids
-
-        try:
-            with transaction.atomic():
-                RolePermission.objects.filter(role=role, permission_id__in=permissions_to_update_ids).update(
-                    allowed=True)
-
-                RolePermission.objects.filter(role=role, permission_id__in=permissions_to_deny_ids).update(
-                    allowed=False)
-
-                new_role_permissions = [
-                    RolePermission(role=role, permission=permission, allowed=True)
-                    for permission in permissions_to_add
-                ]
-                RolePermission.objects.bulk_create(new_role_permissions)
-        except IntegrityError as e:
-            raise serializers.ValidationError(f"Error updating permissions: {str(e)}")
 
 
 class ResponseLoginSerializer(serializers.Serializer):
